5f28f10adb88bc903486584354ecbb8e2f6fc864
Xinhe Huang <xinhe.huagn@stonybrook.edu>
2016-02-11 14:42:30 -0500

    challenge

---
+++ inc/color.h
@@ -0,0 +1,13 @@
+#ifndef JOS_INC_COLOR_H
+#define JOS_INC_COLOR_H
+
+#define BLUE 0x0100;
+#define GREEN 0x0200;
+#define RED 0x0400;
+#define WHITE 0x0700;
+#define YELLOW 0x0600;
+#define BLACK 0X0000;
+
+int current_color;
+
+#endif /* !JOS_INC_COLOR_H */
